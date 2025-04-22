7a238a07bb85aba9342984c7c8133dfc3409571d
나연 <kny5579@naver.com>
2025-04-22 12:36:32 +0900

    init

---
+++ src/main/java/com/couponmoa/backend/couponmoauser/CouponmoaUserApplication.java
@@ -0,0 +1,13 @@
+package com.couponmoa.backend.couponmoauser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CouponmoaUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CouponmoaUserApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/couponmoa/backend/couponmoauser/CouponmoaUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.couponmoa.backend.couponmoauser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CouponmoaUserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
