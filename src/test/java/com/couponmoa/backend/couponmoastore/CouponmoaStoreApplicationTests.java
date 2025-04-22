df3cd3677773a116ac80fc0b13b67cc286bb71af
plusmate <psejin1478@gmail.com>
2025-04-22 11:20:59 +0900

    first commit

---
+++ src/test/java/com/couponmoa/backend/couponmoastore/CouponmoaStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.couponmoa.backend.couponmoastore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CouponmoaStoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
