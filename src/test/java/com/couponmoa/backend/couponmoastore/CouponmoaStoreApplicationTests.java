ef86a97babaebbda7da8955594a7a1f100196732
plusmate <psejin1478@gmail.com>
2025-04-26 22:12:36 +0900

    feat(#3):서버 배포 파이프라인 구현

--- src/test/java/com/couponmoa/backend/couponmoastore/CouponmoaStoreApplicationTests.java
+++ src/test/java/com/couponmoa/backend/couponmoastore/CouponmoaStoreApplicationTests.java
@@ -2,8 +2,10 @@ package com.couponmoa.backend.couponmoastore;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class CouponmoaStoreApplicationTests {
 
     @Test
