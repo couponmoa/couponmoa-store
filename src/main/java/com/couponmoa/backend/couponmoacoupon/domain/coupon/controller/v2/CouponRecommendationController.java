887485ec385a4c0f90ee862f4e852fcbaa97d089
ksmamks <minasithil33@gmail.com>
2025-05-02 18:15:11 +0900

    commit before fix

--- src/main/java/com/couponmoa/backend/couponmoacoupon/domain/coupon/controller/v2/CouponRecommendationController.java
+++ src/main/java/com/couponmoa/backend/couponmoacoupon/domain/coupon/controller/v2/CouponRecommendationController.java
@@ -1,26 +1,26 @@
-package com.couponmoa.backend.couponmoacoupon.domain.coupon.controller.v2;
-
-import com.couponmoa.backend.couponmoacoupon.domain.coupon.entity.Search;
-import com.couponmoa.backend.couponmoacoupon.domain.coupon.service.v2.CouponRecommendationService;
-import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.io.IOException;
-import java.util.List;
-
-@RestController
-@RequestMapping("/api/v1/recommend")
-@RequiredArgsConstructor
-public class CouponRecommendationController {
-
-    private final CouponRecommendationService recommendationService;
-
-    @GetMapping
-    public List<Search> getRecommendations(@RequestParam(name = "userId") String userId) throws IOException {
-        return recommendationService.getAIRecommendations(userId);
-    }
-}
-
+//package com.couponmoa.backend.couponmoacoupon.domain.coupon.controller.v2;
+//
+//import com.couponmoa.backend.couponmoacoupon.domain.coupon.entity.Search;
+//import com.couponmoa.backend.couponmoacoupon.domain.coupon.service.v2.CouponRecommendationService;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RequestParam;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//import java.io.IOException;
+//import java.util.List;
+//
+//@RestController
+//@RequestMapping("/api/v1/recommend")
+//@RequiredArgsConstructor
+//public class CouponRecommendationController {
+//
+//    private final CouponRecommendationService recommendationService;
+//
+//    @GetMapping
+//    public List<Search> getRecommendations(@RequestParam(name = "userId") String userId) throws IOException {
+//        return recommendationService.getAIRecommendations(userId);
+//    }
+//}
+//
