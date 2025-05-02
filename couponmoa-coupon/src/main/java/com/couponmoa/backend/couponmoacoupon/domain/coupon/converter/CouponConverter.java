3ff309e7286706e73d49ebf370220bddfbb2f581
ksmamks <minasithil33@gmail.com>
2025-05-03 07:37:02 +0900

    fix : elasticsearch 주석처리, 모듈 분리

--- couponmoa-coupon/src/main/java/com/couponmoa/backend/couponmoacoupon/domain/coupon/converter/CouponConverter.java
+++ couponmoa-coupon/src/main/java/com/couponmoa/backend/couponmoacoupon/domain/coupon/converter/CouponConverter.java
@@ -1,21 +1,21 @@
-package com.couponmoa.backend.couponmoacoupon.domain.coupon.converter;
-
-import com.couponmoa.backend.couponmoacoupon.domain.coupon.entity.Coupon;
-import com.couponmoa.backend.couponmoacoupon.domain.coupon.entity.Search;
-
-public class CouponConverter {
-
-    // Coupon 객체를 Elasticsearch 문서 형식인 Search 객체로 변환
-    public static Search toSearchDocument(Coupon coupon) {
-        return Search.builder()
-                .couponId(coupon.getId())
-                .name(coupon.getName())
-                .description(coupon.getDescription())
-                .discountAmount(coupon.getDiscountAmount())
-                .discountRate(coupon.getDiscountRate())
-                .minOrderAmount(coupon.getMinOrderAmount())
-                .maxDiscountAmount(coupon.getMaxDiscountAmount())
-                .expiryDate(coupon.getExpiryDate().toString())  // 날짜를 String으로 변환
-                .build();
-    }
-}
+//package com.couponmoa.backend.couponmoacoupon.domain.coupon.converter;
+//
+//import com.couponmoa.backend.couponmoacoupon.domain.coupon.entity.Coupon;
+//import com.couponmoa.backend.couponmoacoupon.domain.coupon.entity.Search;
+//
+//public class CouponConverter {
+//
+//    // Coupon 객체를 Elasticsearch 문서 형식인 Search 객체로 변환
+//    public static Search toSearchDocument(Coupon coupon) {
+//        return Search.builder()
+//                .couponId(coupon.getId())
+//                .name(coupon.getName())
+//                .description(coupon.getDescription())
+//                .discountAmount(coupon.getDiscountAmount())
+//                .discountRate(coupon.getDiscountRate())
+//                .minOrderAmount(coupon.getMinOrderAmount())
+//                .maxDiscountAmount(coupon.getMaxDiscountAmount())
+//                .expiryDate(coupon.getExpiryDate().toString())  // 날짜를 String으로 변환
+//                .build();
+//    }
+//}
