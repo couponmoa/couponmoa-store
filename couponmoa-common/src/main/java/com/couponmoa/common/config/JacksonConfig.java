82c744129d4256a2d526271562ebe7bef72d0fe8
ksmamks <minasithil33@gmail.com>
2025-05-04 21:48:16 +0900

    logging debug

--- couponmoa-common/src/main/java/com/couponmoa/common/config/JacksonConfig.java
+++ couponmoa-common/src/main/java/com/couponmoa/common/config/JacksonConfig.java
@@ -1,7 +1,10 @@
 package com.couponmoa.common.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -16,4 +19,15 @@ public class JacksonConfig {
             builder.featuresToDisable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
         };
     }
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @PostConstruct
+    public void checkObjectMapper() {
+        boolean isTimestamps = objectMapper.getSerializationConfig()
+                .isEnabled(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+        System.out.println("🚨 WRITE_DATES_AS_TIMESTAMPS is enabled? " + isTimestamps);
+    }
+}
 }
