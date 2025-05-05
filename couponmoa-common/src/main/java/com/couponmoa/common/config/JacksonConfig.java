de30f1eade2753fd7e97c5401944dcc32e2664a3
ksmamks <minasithil33@gmail.com>
2025-05-05 22:23:39 +0900

    refactor : common 모듈 분리

--- couponmoa-common/src/main/java/com/couponmoa/common/config/JacksonConfig.java
+++ couponmoa-common/src/main/java/com/couponmoa/common/config/JacksonConfig.java
@@ -1,29 +1,29 @@
-package com.couponmoa.common.config;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-@Configuration
-public class JacksonConfig {
-
-    private static final Logger logger = LoggerFactory.getLogger(JacksonConfig.class);
-
-    @Bean
-    public ObjectMapper objectMapper() {
-        ObjectMapper mapper = new ObjectMapper();
-        mapper.registerModule(new JavaTimeModule());
-        mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS); // WRITE_DATES_AS_TIMESTAMPS 비활성화
-
-        // 로그로 WRITE_DATES_AS_TIMESTAMPS 설정 상태 확인
-        boolean isDisabled = mapper.getSerializationConfig().isEnabled(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
-        logger.info("WRITE_DATES_AS_TIMESTAMPS is disabled? {}", !isDisabled); // false이면 정상
-
-        return mapper;
-    }
-}
+//package com.couponmoa.common.config;
+//
+//import com.fasterxml.jackson.databind.ObjectMapper;
+//import com.fasterxml.jackson.databind.SerializationFeature;
+//import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
+//
+//@Configuration
+//public class JacksonConfig {
+//
+//    private static final Logger logger = LoggerFactory.getLogger(JacksonConfig.class);
+//
+//    @Bean
+//    public ObjectMapper objectMapper() {
+//        ObjectMapper mapper = new ObjectMapper();
+//        mapper.registerModule(new JavaTimeModule());
+//        mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS); // WRITE_DATES_AS_TIMESTAMPS 비활성화
+//
+//        // 로그로 WRITE_DATES_AS_TIMESTAMPS 설정 상태 확인
+//        boolean isDisabled = mapper.getSerializationConfig().isEnabled(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+//        logger.info("WRITE_DATES_AS_TIMESTAMPS is disabled? {}", !isDisabled); // false이면 정상
+//
+//        return mapper;
+//    }
+//}
